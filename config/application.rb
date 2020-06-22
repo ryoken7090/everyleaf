5f0d6f0df8f5e3a9642505c3b97b373a479395ab
ryoken nakayama <ryoken7090@gmail.com>
2020-06-22 15:10:07 +0900

    rails g modelコマンドでspec/models/**.specファイルとspec/factories/**.specファイルのみが生成されるよう追記

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,13 @@ module ElApp
     config.generators do |g|
       g.assets false
       g.helper false
+      g.test_framework :rspec,
+                   model_specs: true,
+                   view_specs: false,
+                   helper_specs: false,
+                   routing_specs: false,
+                   controller_specs: false,
+                   request_specs: false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
