e2be8b5afc02e6a3d2e211a517f033d2c47a34e4
ryoken nakayama <ryoken7090@gmail.com>
2020-06-24 15:50:50 +0900

    before,factorybotを使って処理とデータの共通化

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+  end
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
-        FactoryBot.create(:task, title: 'task')
         visit tasks_path
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'Factoryで作ったデフォルトのタイトル1'
       end
     end
     context '複数のタスクを作成した場合' do
       it 'タスクが作成日時の順に並んでいる' do
-        FactoryBot.create(:task, title: 'task')
         FactoryBot.create(:task, title: 'new_task')
         visit tasks_path
         task_list = all('.task_row')
         expect(task_list[0]).to have_content 'new_task'
-        expect(task_list[1]).to have_content 'task'
+        expect(task_list[2]).to have_content 'Factoryで作ったデフォルトのコンテント1'
       end
     end
   end
@@ -23,10 +25,10 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '必要項目を入力して、createボタンを押した場合' do
       it 'データが保存される' do
         visit new_task_path
-        fill_in 'タイトル', with: 'テストのタイトル'
-        fill_in '内容', with: 'コンテンツ'
+        fill_in 'タイトル', with: '入力したタイトル'
+        fill_in '内容', with: '入力したコンテンツ'
         click_button 'Submit'
-        expect(page).to have_content 'テストのタイトル'
+        expect(page).to have_content '入力したタイトル'
       end
     end
   end
