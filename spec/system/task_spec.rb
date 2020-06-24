4f4044a917fe4f79fc7c2e2a269b45b3d83c0f17
ryoken nakayama <ryoken7090@gmail.com>
2020-06-24 15:06:55 +0900

    タスクが降順にならぶことの確認テストの追加および降順になった事によるテストの修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,9 +3,19 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
-        FactoryBot.create(:task)
+        FactoryBot.create(:task, title: 'task')
         visit tasks_path
-        expect(page).to have_content 'test_content'
+        expect(page).to have_content 'task'
+      end
+    end
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の順に並んでいる' do
+        FactoryBot.create(:task, title: 'task')
+        FactoryBot.create(:task, title: 'new_task')
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'new_task'
+        expect(task_list[1]).to have_content 'task'
       end
     end
   end
@@ -13,8 +23,8 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '必要項目を入力して、createボタンを押した場合' do
       it 'データが保存される' do
         visit new_task_path
-        fill_in 'Title', with: 'テストのタイトル'
-        fill_in 'Content', with: 'コンテンツ'
+        fill_in 'タイトル', with: 'テストのタイトル'
+        fill_in '内容', with: 'コンテンツ'
         click_button 'Submit'
         expect(page).to have_content 'テストのタイトル'
       end
@@ -26,7 +36,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         FactoryBot.create(:task, content: 'target task')
         5.times {FactoryBot.create(:task)}
         visit tasks_path
-        first('tbody tr').click_link 'Show'
+        all('tbody tr')[5].click_link 'Show'
         expect(page).to have_content 'target task'
       end
     end
