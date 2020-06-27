126ab147980a37dfbe834dc39febddf010166107
ryoken nakayama <ryoken7090@gmail.com>
2020-06-28 01:37:23 +0900

    あいまい検索機能実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,5 @@ class Task < ApplicationRecord
   validates :content, presence: true
   enum status: { yet: 0, working: 1, completed: 2 }
 
-  scope :search, ->(search_title) { where("title LIKE ?", search_title) }
+  scope :search, ->(search_title) { where("title LIKE ?", "%#{search_title}%") }
 end
