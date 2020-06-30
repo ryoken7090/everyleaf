7079f553680591cbc15d617ea99c97b54b727fd6
ryoken nakayama <ryoken7090@gmail.com>
2020-06-30 11:26:39 +0900

    tasksにpriorityカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_070944) do
+ActiveRecord::Schema.define(version: 2020_06_30_022331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_06_26_070944) do
     t.datetime "updated_at", null: false
     t.datetime "expired_at", default: "2020-07-02 14:31:45", null: false
     t.integer "status", default: 0
+    t.integer "priority", default: 0
     t.index ["title"], name: "index_tasks_on_title"
   end
 
