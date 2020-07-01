0cf5e8ead54df3462122911c5b10e40d80a3d341
ryoken nakayama <ryoken7090@gmail.com>
2020-07-01 11:53:00 +0900

    ユーザ登録フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'users/new'
   root to: 'tasks#index'
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
+
 end
