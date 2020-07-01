9c001b4c1650e41a539d0ba14c185047efb0aaa3
ryoken nakayama <ryoken7090@gmail.com>
2020-07-01 15:12:49 +0900

    タスクとユーザーのためタスクテーブルにユーザーの外部キー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_023113) do
+ActiveRecord::Schema.define(version: 2020_07_01_060857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2020_07_01_023113) do
     t.datetime "expired_at", default: "2020-07-02 14:31:45", null: false
     t.integer "status", default: 0
     t.integer "priority", default: 0
+    t.bigint "user_id"
     t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_07_01_023113) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
