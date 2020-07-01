597bbd9f98407bfa9df6651d43f686b84bae4d82
ryoken nakayama <ryoken7090@gmail.com>
2020-07-01 13:40:26 +0900

    userのshow action追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'users/new'
   root to: 'tasks#index'
   resources :tasks
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
 end
