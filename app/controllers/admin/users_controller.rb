c295897e9d88b4be800717ea2e6a4f4b3682488c
ryoken nakayama <ryoken7090@gmail.com>
2020-07-02 18:04:48 +0900

    管理者の新規ユーザー登録機能を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,12 +4,26 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
-  def show
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 管理者画面のユーザー一覧画面に遷移
+      redirect_to admin_users_path
+    end
+  end
+
+  def show
   end
 
   private
   def set_user
     @user = User.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
   end
 end
