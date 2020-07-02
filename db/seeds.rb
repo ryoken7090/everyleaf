e6415b3e293142ee8eea1f5249fd15a189a46fe5
ryoken nakayama <ryoken7090@gmail.com>
2020-07-02 14:06:49 +0900

    userに管理者adminカラムを追加・seedでadminユーザーの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,26 @@
-5.times do |n|
-  User.create!(
-    email: "test#{n + 1}@example.com",
-    name: "テスト#{n + 1}",
-    password: "password",
-    password_confirmation: "password"
-  )
-end
-User.all.each do |user|
-  20.times do |n|
-    user.tasks.create!(
-      title: 'タイトル',
-      content: '内容はこれ！',
-      status: "着手中",
-      priority: "中",
-      user_id: user.id
-    )
-  end
-end
+# 5.times do |n|
+#   User.create!(
+#     email: "test#{n + 1}@example.com",
+#     name: "テスト#{n + 1}",
+#     password: "password",
+#     password_confirmation: "password"
+#   )
+# end
+# User.all.each do |user|
+#   20.times do |n|
+#     user.tasks.create!(
+#       title: 'タイトル',
+#       content: '内容はこれ！',
+#       status: "着手中",
+#       priority: "中",
+#       user_id: user.id
+#     )
+#   end
+# end
+User.create!(
+  email: "admin1@example.com",
+  name: "管理者1",
+  password: "password",
+  password_confirmation: "password",
+  admin: true
+)
