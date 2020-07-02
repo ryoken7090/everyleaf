098e58ba9525e6c323736fde62d102715dae817d
ryoken nakayama <ryoken7090@gmail.com>
2020-07-02 16:35:08 +0900

    管理画面でユーザ一覧・ユーザー詳細機能を作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,15 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
+  namespace :admin do
+    resources :users
+  end
   root to: 'tasks#index'
   resources :tasks
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
-  namespace :admin do
-    resource :users, only: [:index]
-  end
+
 end
