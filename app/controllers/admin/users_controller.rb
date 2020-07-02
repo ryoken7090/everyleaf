5850f87d74550c9a8228011bf34ae20f96fd603a
ryoken nakayama <ryoken7090@gmail.com>
2020-07-03 01:47:49 +0900

    各ユーザーのタスク数を表示

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   def index
-    @users = User.all
+    @users = User.includes(:tasks).all
   end
 
   def new
