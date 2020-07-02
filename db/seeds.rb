f5f7fcdf8f212b6286ea04bb57c9552df46ce6fe
ryoken nakayama <ryoken7090@gmail.com>
2020-07-02 13:41:39 +0900

    seedで初期データの準備

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |n|
+  User.create!(
+    email: "test#{n + 1}@example.com",
+    name: "テスト#{n + 1}",
+    password: "password",
+    password_confirmation: "password"
+  )
+end
+User.all.each do |user|
+  20.times do |n|
+    user.tasks.create!(
+      title: 'タイトル',
+      content: '内容はこれ！',
+      status: "着手中",
+      priority: "中",
+      user_id: user.id
+    )
+  end
+end
