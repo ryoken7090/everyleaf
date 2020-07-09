d6da926f3bedc9cdb3bd7d98ea1f643e030a6222
ryoken nakayama <ryoken7090@gmail.com>
2020-07-09 16:34:21 +0900

    rootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :tasks
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
-
+  root to: "tasks#index"
 end
